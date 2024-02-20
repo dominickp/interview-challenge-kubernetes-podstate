8293a5e8be2c1ae0418101e38c7ceed4b828edee
Dom <peluso.dominick@gmail.com>
2024-02-20 16:54:28 -0500

    Cleanup

--- src/kube_client.py
+++ src/kube_client.py
@@ -10,25 +10,32 @@ class KubeClient:
     def __init__(self) -> None:
         configuration = client.Configuration()
         if SECRET_KUBE_API_KEY:
+            # For local development with docker-compose, run .\scripts\get-api-key.ps1 
+            # and save the API key in the .env file like this:
+            #   SECRET_KUBE_API_KEY=the-api-key
+            #   KUBERNETES_SERVICE_HOST=192.168.1.68    # I couldn't get the docker DNS name of k0s to work
+            #   KUBERNETES_SERVICE_PORT=6443
             configuration.api_key['authorization'] = f"Bearer {SECRET_KUBE_API_KEY}"
             configuration.host = f"https://{KUBERNETES_SERVICE_HOST}:{KUBERNETES_SERVICE_PORT}"
             configuration.verify_ssl = False
             with client.ApiClient(configuration) as api_client:
                 self.api = client.CoreV1Api(api_client)
         else:
+            # Load the in-cluster configuration when running on actual Kubernetes
             config.load_incluster_config()
             self.api = client.CoreV1Api()
 
-    def _matches_pod_state(self, pod: dict, pod_state: str) -> bool:
-        return True
-    
-    def get_pods(self, namespace: str):
+    def get_pods(self, namespace: str) -> list:
+        """"
+        Returns a list of pods in the specified namespace.
 
-        # List all pods in the default namespace that have the matching pod state
+        Parameters:
+        namespace (str): The namespace to list pods from.
+
+        Returns:
+        list: A list of Kubernetes pods.
+        """
         podlist = self.api.list_namespaced_pod(namespace=namespace)
         items = podlist.to_dict().get("items", [])
-        # print(items)
-        # Filter the list of pods to only include those with the matching pod state
         return items
     
-  
--- src/main.py
+++ src/main.py
@@ -6,19 +6,14 @@ from fastapi import FastAPI
 from kube_client import KubeClient
 from response import get_podlist_table, VALID_POD_STATUSES
 
-POD_STATUS = os.environ.get("POD_STATUS", "Error")
+POD_STATUS = os.environ.get("POD_STATUS", "Running")
 if POD_STATUS not in VALID_POD_STATUSES:
     raise ValueError(f"Invalid pod status: '{POD_STATUS}' must be one of: {VALID_POD_STATUSES}")
 
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World2"}
-
-
-@app.get("/kube")
-def read_kube():
+def get_pods_table():
     client = KubeClient()
     try:
         pods = client.get_pods("default")
@@ -26,9 +21,3 @@ def read_kube():
     except Exception as e:
         logging.exception(e)
         return {"error": str(e)}
-    
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
